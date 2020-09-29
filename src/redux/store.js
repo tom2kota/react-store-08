74b45587dc1e3af912a64d1b1418534139f68244
Web Dev <tom2kota@gmail.com>
2020-09-30 00:22:30 +0300

    Add root saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware} from "redux";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 import createSagaMiddleware from "redux-saga";
-import {fetchCollectionsStart} from "./shop/shopSagas";
+import rootSaga from "./rootSaga";
 
 const saga = createSagaMiddleware()
 const middlewares = [saga]
@@ -13,4 +13,4 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-saga.run(fetchCollectionsStart)
+saga.run(rootSaga)
